44cc0ee3f5fbb5d79296cf3e3ce7650451c63ecc
Anthony Floyd <anthonyfloyd@gmail.com>
2016-12-02 17:11:50 -0800

    Completely did algorithm to accommodate part 2 keypad design. Part 2 works!

--- day02.jl
+++ day02.jl
@@ -8,20 +8,13 @@ println("Day 2: Bathroom Security")
 println("")
 
 DEBUG = false
-KEYPAD = [[1, 2, 3],[4,5,6],[7,8,9]]
+KEYPAD_PART1 = [['0','0','0','0','0'],['0', '1', '2', '3', '0'],['0','4','5','6','0'],['0','7','8','9','0'],['0','0','0','0','0']]
+KEYPAD_PART2 = [['0','0','0','0','0','0','0'],['0', '0', '0', '1', '0', '0', '0'],['0','0','2','3','4','0','0'],['0','5','6','7','8','9','0'],['0','0','A','B','C','0','0'],['0','0','0','D','0','0','0'],['0','0','0','0','0','0','0']]
 
-if DEBUG println("Keypad\n",KEYPAD) end
-#
-# Keypad looks like this:
-#
-# 1 2 3
-# 4 5 6
-# 7 8 9
-#
-function getNumbers(inputDataBits, isPart2)
+function getNumbers(inputDataBits, keypad)
   # do nothing yet
-  numbers = Vector{Int}()
-  currentLocation = [2,2] # start at number 5
+  numbers = Vector{Char}()
+  currentLocation = [3,3] # start at number 5
 
   for controlString in inputDataBits
     if DEBUG println("controlString: $controlString") end
@@ -32,22 +25,22 @@ function getNumbers(inputDataBits, isPart2)
 
       if direction == 'U'
         if DEBUG println("Up from ", currentLocation[1]) end
-        if currentLocation[1] > 1
+        if keypad[currentLocation[1]-1][currentLocation[2]] != '0'
           currentLocation[1] -= 1
         end
       elseif direction == 'D'
         if DEBUG println("Down from ", currentLocation[1]) end
-        if currentLocation[1] < 3
+        if keypad[currentLocation[1]+1][currentLocation[2]] != '0'
           currentLocation[1] += 1
         end
       elseif direction == 'R'
         if DEBUG println("Right from ", currentLocation[2]) end
-        if currentLocation[2] < 3
+        if keypad[currentLocation[1]][currentLocation[2]+1] != '0'
           currentLocation[2] += 1
         end
       elseif direction == 'L'
         if DEBUG println("Left from ", currentLocation[2]) end
-        if currentLocation[2] > 1
+        if keypad[currentLocation[1]][currentLocation[2]-1] != '0'
           currentLocation[2] -= 1
         end
       else
@@ -58,7 +51,7 @@ function getNumbers(inputDataBits, isPart2)
     if DEBUG println("Current location: ", currentLocation) end
     if DEBUG println("Keypad number: ", KEYPAD[currentLocation[1]][currentLocation[2]]) end
 
-    push!(numbers,KEYPAD[currentLocation[1]][currentLocation[2]])
+    push!(numbers,keypad[currentLocation[1]][currentLocation[2]])
   end
 
   return numbers
@@ -84,5 +77,8 @@ map!(strip,inputDataBits)
 
 if DEBUG println("inputDataBits\n", inputDataBits) end
 
-part1Code = getNumbers(inputDataBits, true)
+part1Code = getNumbers(inputDataBits, KEYPAD_PART1)
 println("The part1 code is ", part1Code)
+
+part2Code = getNumbers(inputDataBits, KEYPAD_PART2)
+println("The part2 code is ", part2Code)
