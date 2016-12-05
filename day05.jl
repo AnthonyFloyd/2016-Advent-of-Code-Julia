cece18079b77a9b2371bed64ca98d3f4df82c7ec
Anthony Floyd <anthonyfloyd@gmail.com>
2016-12-04 22:52:36 -0800

    Day 5 part 2 solution

--- day05.jl
+++ day05.jl
@@ -8,6 +8,7 @@ using Nettle
 println("Advent of Code")
 println("Day 5: How About a Nice Game of Chess?")
 println("")
+println("Part 1")
 
 #DOOR_ID = "abc"
 DOOR_ID = "ojvtpuvg"
@@ -22,9 +23,37 @@ while foundCounter < 8
   if testHash[1:5] == "00000"
     foundCounter += 1
     password = password * string(testHash[6])
+    println("$password")
   end
 
   counter += 1
 end
 
-println("The password for $DOOR_ID_EX is $password.")
+println("The part 1 password for $DOOR_ID is $password.")
+
+# part 2
+println("\nPart2")
+
+notFound = true
+counter = 0
+foundCounter = 0
+password = "--------"
+
+while foundCounter < 8
+  testHash = hexdigest("md5",DOOR_ID * string(counter))
+  if testHash[1:5] == "00000"
+    if isnumber(testHash[6])
+      position = parse(string(testHash[6])) + 1
+      if position <= 8
+        if password[position] == '-'
+          password = password[1:position-1] * string(testHash[7]) * password[position+1:end]
+          foundCounter += 1
+          println("$password")
+        end
+      end
+    end
+  end
+  counter += 1
+end
+
+println("The part 2 password for $DOOR_ID is $password.")
